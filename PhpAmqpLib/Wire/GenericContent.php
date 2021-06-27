d499631387a245c645960385dbfff9e8bcde2635
Junrui Chen <jchen@freelancer.com>
2021-06-28 09:17:48 +1000

    Change the type annotation of  from AMQPChannel[] to array

--- PhpAmqpLib/Wire/GenericContent.php
+++ PhpAmqpLib/Wire/GenericContent.php
@@ -9,7 +9,7 @@ use PhpAmqpLib\Channel\AMQPChannel;
  */
 abstract class GenericContent
 {
-    /** @var AMQPChannel[] */
+    /** @var array */
     public $delivery_info = array();
 
     /** @var array Final property definitions */
